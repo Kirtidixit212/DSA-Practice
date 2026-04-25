ad3352970b4191cf7df261fd15709ecf97885171
Kirti Dixit <kirtidixit052@gmail.com>
2026-04-25 17:39:45 +0000

    day : 4

--- Sorting.py
+++ Sorting.py
@@ -23,4 +23,18 @@ def selectionSort(nums):
             if nums[j] < nums[min]:
                 min = j
         nums[min],nums[i] = nums[i],nums[min]
-    return nums            
+    return nums
+    
+# INSERTION SORT
+def insertion_sort(nums):
+    n = len(nums)
+
+    for i in range(1,n):
+        curr = nums[i]
+        prev = i - 1
+        
+        while prev >= 0 and nums[prev] > curr:
+            arr[prev+1] = arr[prev]
+            prev -= 1
+        nums[prev +1] = curr
+    return num        
